e95186e09219f7b7c87ce49404a399b374817f01
Lele Li <lilelr@163.com>
2018-04-23 21:49:04 +0800

    chapter 5 chapter 14 two phase termination chapter 4 Guared Suspension

--- src/main/java/twophasetermination/AlarmAgent.java
+++ src/main/java/twophasetermination/AlarmAgent.java
@@ -1,5 +1,12 @@
 package twophasetermination;
 
+import guardsuspension.Blocker;
+import guardsuspension.ConditionVarBlocker;
+import guardsuspension.GuardedAction;
+import guardsuspension.Predicate;
+
+import java.util.Timer;
+
 /**
  * Created by yuxiao on 4/23/18.
  */
@@ -7,5 +14,28 @@ public class AlarmAgent {
 
     private volatile boolean connectedToServer = false;
 
+    private final Predicate agentConnected = new Predicate() {
+        @Override
+        public boolean evaluate() {
+            return  connectedToServer;
+        }
+    };
 
+    private final Blocker blocker = new ConditionVarBlocker();
+
+    private final Timer heartbeatTimer = new Timer(true);
+
+
+    public void sendAlarm(AlarmInfo alarm)throws Exception{
+        GuardedAction<Void> guardedAction = new GuardedAction<Void>(agentConnected) {
+            @Override
+            public Void call() throws Exception {
+
+                return null;
+            }
+        };
+
+        blocker.callWithGuard(guardedAction);
+
+    }
 }
